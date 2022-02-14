5eff2115705ff3a26fde9fea2474575c62d39093
subham surjyakanta <subhamsurjyakanta564@gmail.com>
2022-02-14 22:17:48 +0530

    social-media-Integration

---
+++ app/src/main/java/com/example/firebaseauthall/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.firebaseauthall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.identity.SignInClient;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+            }
+
+}
